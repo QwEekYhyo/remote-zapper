d1d75b0e512d34144fba23adec3940f3fbbf2d23
QwEekYhyo <qweekyhyo.yt83@gmail.com>
2023-08-20 14:46:46 +0200

    basic server and client

---
+++ server.py
@@ -0,0 +1,14 @@
+# Echo server program
+import socket
+
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 50007              # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(5)
+while True:
+    conn, addr = s.accept()
+    print('Connected by', addr)
+    data = conn.recv(1024)
+    if not data: break
+    conn.sendall(data)
